1391a5c7514a9104b1afa236941412f5bc27845e
ThereSiaMDH <77191120+ThereSiaMDH@users.noreply.github.com>
2021-01-09 12:27:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = ("express");
+const app = express();
+
 console.log("hello world")
 
 //TO DO: More code
+
+
+app.get("/", (req,res) => {
+    res.send("hello world");
+});
+
+app.listen(3000, 
+    () => console.log("Example app running"));
